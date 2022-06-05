f18fbdc23a15e1eb13f81cd58f8b3236a2e2fd1d
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-05 17:17:59 -0300

    List users, MiddleWare, Login

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 
+const AuthMidleware = require('./Middlewares/AuthMiddleware');
 const UsersController = require('./controllers/UsersController');
+const LoginController = require('./controllers/LoginController');
 
 const routes = new Router();
 
-routes.get('/users', UsersController.list);
+routes.get('/users', AuthMidleware, UsersController.list);
 routes.post('/users', UsersController.create);
+
+routes.post('/login', LoginController.index);
 
 module.exports = routes;
