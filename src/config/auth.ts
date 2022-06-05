4234e0c9716ab60d2281b6d56f3c3df4cc2e5961
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-05 14:13:35 -0300

    Auth Config

---
+++ src/config/auth.ts
@@ -0,0 +1,5 @@
+// exports a 64 character string and a expiration time.
+module.exports = {
+  secret: '768468bc943db78780fbc167461279c7ebd38f8a70de3381e66a512314385c99ab63acf71e534effeeff8d92e55e7c0e8073467d82308ea56e627a2d2a44f4de',
+  expiresIn: '6h',
+};
